66508a96eab1bafa525b1e03339b4edf9b7a0f96
yingaoyun <610624354@qq.com>
2021-10-25 11:20:03 +0800

    js

---
+++ meitu/app/page/statistics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+      (function () {
+          var hm = document.createElement("script");
+          hm.src = "https://v1.cnzz.com/z_stat.php?id=1279367758&web_id=1279367758";
+          var s = document.getElementsByTagName("script")[0];
+          s.parentNode.insertBefore(hm, s);
+      })();
